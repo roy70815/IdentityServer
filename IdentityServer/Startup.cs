ad902136834534bdc6c08bed43c5ad4e722ef065
冠榮 邱 <roy70815@gmail.com>
2021-09-10 22:16:26 +0800

    結合IdentityServer4快速測試UI

--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,17 +14,41 @@ namespace IdentityServer
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //var connection = Configuration.GetConnectionString("DBConnection");
+            //var builder = services.AddIdentityServer()
+            //    .AddConfigurationStore(opt =>
+            //    {
+            //        opt.ConfigureDbContext = context =>
+            //        {
+            //            context.UseMySql(connection, ServerVersion.AutoDetect(connection), sql=> 
+            //            {
+            //                sql.MigrationsAssembly("IdentityServer");
+            //            });
+            //        };
+            //    });
+
             services.AddIdentityServer()
             // 方便測試選擇in-memory
             .AddInMemoryClients(Clients.GetClients())
             .AddInMemoryApiResources(Resources.GetApiResources())
             .AddInMemoryApiScopes(Resources.GetApiScopes())
+            .AddTestUsers(TestUsers.GetUsers().ToList())
             // 方便開發階段於啟動時產生暫時密鑰(tempkey.jwk)
             .AddDeveloperSigningCredential();
+
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,17 +59,27 @@ namespace IdentityServer
                 app.UseDeveloperExceptionPage();
             }
 
+            //app.UseRouting();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
+            // 啟用IdentityServer
+            app.UseIdentityServer();
+            //for QuickStart-UI 啟用靜態檔案
+            app.UseStaticFiles();
             app.UseRouting();
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}");
             });
-            // 啟用IdentityServer
-            app.UseIdentityServer();
         }
     }
 }
---
+++ IdentityServer/TestUsers.cs
@@ -0,0 +1,40 @@
+﻿using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    public class TestUsers
+    {
+        /// <summary>
+        /// Define which uses will use this IdentityServer
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<TestUser> GetUsers()
+        {
+            return new[]
+            {
+                new TestUser
+                {
+                    SubjectId = "10001",
+                    Username = "test1@hotmail.com",
+                    Password = "test1password"
+                },
+                new TestUser
+                {
+                    SubjectId = "10002",
+                    Username = "test2@hotmail.com",
+                    Password = "test2password"
+                },
+                new TestUser
+                {
+                    SubjectId = "10003",
+                    Username = "test3@hotmail.com",
+                    Password = "test3password"
+                }
+            };
+        }
+    }
+}
